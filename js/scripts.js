a2aed66e7c30b7c43b08382880ad5e0768f900a5
unknown <gabe.c.monteiro@gmail.com>
2019-11-08 10:13:32 -0800

    add selectors and additional logic. still not functioning.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 //---Business Logic
 var priceArr = [];
-function Order (size){
+function Order (size) {
   this.size = size;
   this.veggie1 = 0.5;
   this.veggie2 = 0.5;
   this.veggie3 = 0.5;
   this.meat1 = 1;
-  this.meat2 = 1,
+  this.meat2 = 1;
 }
 Order.prototype.pizzaCost = function () {
-  if (this.size === "Medium")
+  if (this.size === "Medium") {
     this.pizzaPrice += 8;
   } else if (this.size === "Large") {
     this.pizzaPrice += 10;
@@ -18,13 +18,25 @@ Order.prototype.pizzaCost = function () {
   this.pizzaPrice += this.veggie2;
   this.pizzaPrice += this.veggie3;
   this.pizzaPrice += this.meat1;
+  this.pizzaPrice += this.meat2;
   return this.pizzaPrice;
 };
 Order.prototype.totalCost = function () {
   var orderTotal = 0;
-  for ()
+  for (var arrayElement = 0; arrayElement < priceArr.length; arrayElement ++) {
+    orderTotal += priceArr[arrayElement];
 }
 return orderTotal;
 };
 
 //---User End Logic
+$(document).ready(function(event) {
+  var size = $("select#sizeSelect").val();
+  var veggie1 = $("select#veggieSelect1").val();
+  var veggie2 = $("select#veggieSelect2").val();
+  var veggie3 = $("select#veggieSelect3").val();
+  var meat1 = $("select#meatSelect1").val();
+  var meat2 = $("select#meatSelect2").val();
+
+
+});
